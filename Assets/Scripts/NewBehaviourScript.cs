f10e211ba28aaec59920fd09cfc20834fa8b5caa
Nuitfan <108407925+Nuitfan@users.noreply.github.com>
2023-10-11 08:18:08 +0800

    2

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("1");
+        Debug.Log("2");
     }
 
 }
